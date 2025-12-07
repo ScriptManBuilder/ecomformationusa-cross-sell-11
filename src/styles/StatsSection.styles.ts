b177d7c7eeb3feb03e24b0ac28c238087df56faa
ScriptManBuilder <dan.gora2004@gmail.com>
2025-12-07 15:16:03 +0200

    Fix issues, update design, fix errors

--- src/styles/StatsSection.styles.ts
+++ src/styles/StatsSection.styles.ts
@@ -127,6 +127,9 @@ export const StatCard = styled.div<{ $background: string }>`
   position: relative;
   overflow: hidden;
   backdrop-filter: blur(10px);
+  display: flex;
+  flex-direction: column;
+  height: 100%;
 
   &::before {
     content: '';
@@ -207,6 +210,7 @@ export const StatDescription = styled.p`
   margin-top: 0.5rem;
   opacity: 0.7;
   line-height: 1.6;
+  flex: 1;
 `;
 
 export const StatValue = styled.div`
